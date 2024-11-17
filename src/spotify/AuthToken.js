9b3843ce8239244e8bcfdd0d7ce899a4af3e5644
nov-bohdan <nov.bohdan@gmail.com>
2024-11-17 13:44:05 -0600

    Updated spotify auth

---
+++ src/spotify/AuthToken.js
@@ -0,0 +1,16 @@
+const AuthToken = {
+    get accessToken() { return localStorage.getItem('access_token') || null },
+    get expires() { return localStorage.getItem('expires') || null },
+    get refreshToken() { return localStorage.getItem('refresh_token') || null },
+    
+    save(response) {
+        const { access_token, expires_in, refresh_token } = response;
+        const now = new Date();
+        const expireDate = new Date(now.getTime() + (expires_in * 1000));
+        localStorage.setItem('access_token', access_token);
+        localStorage.setItem('expires', expireDate);
+        localStorage.setItem('refresh_token', refresh_token);
+    }
+}
+
+export default AuthToken;
