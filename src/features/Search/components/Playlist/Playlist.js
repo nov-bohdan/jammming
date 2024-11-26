1ee3b7ab3f8d23a75b5167fe7b1db70f068da716
nov-bohdan <nov.bohdan@gmail.com>
2024-11-26 17:01:23 -0600

    Minor refactoring in playlist and tracklist

--- src/features/Search/components/Playlist/Playlist.js
+++ src/features/Search/components/Playlist/Playlist.js
@@ -1,12 +1,22 @@
 import React from "react"
 import styles from "./Playlist.module.css";
-import TrackList from "../TrackList/TrackList";
+import Track from "../Track/Track";
 
 function Playlist({ playlistTracks }) {
+    function onClick(event) {
+        event.preventDefault();
+        // onAddHandle(event.target);
+    }
+
     return (
         <div className={styles.playlist}>
             <h2>Playlist</h2>
-            <TrackList trackList={playlistTracks} />
+            {playlistTracks.map(trackObject => (
+                <div className={styles.track} key={trackObject.id}>
+                    <Track trackObject={trackObject} />
+                    <button className={styles.removeButton} data-trackid={trackObject.id} onClick={onClick}>X</button>
+                </div>
+            ))}
         </div>
     )
 }
--- src/features/Search/components/TrackList/TrackList.js
+++ src/features/Search/components/TrackList/TrackList.js
@@ -2,10 +2,18 @@ import Track from "../Track/Track";
 import styles from './TrackList.module.css';
 
 function TrackList({ trackList, onAddHandle }) {
+    function onClick(event) {
+        event.preventDefault();
+        onAddHandle(event.target);
+    }
+
     return (
         <div className={styles.trackList}>
             {trackList.map(trackObject => (
-                <Track trackObject={trackObject} key={trackObject.id} onAddHandle={onAddHandle} />
+                <div className={styles.track} key={trackObject.id}>
+                    <Track trackObject={trackObject} />
+                    <button className={styles.addButton} data-trackid={trackObject.id} onClick={onClick}>+</button>
+                </div>
             ))}
         </div>
     )
